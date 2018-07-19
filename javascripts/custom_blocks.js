7a429491a78a96be24530694334eb1db6b543d9d
David Liu <msglight4874@gmail.com>
2018-07-19 11:32:40 +0800

    調整積木顏色

--- javascripts/custom_blocks.js
+++ javascripts/custom_blocks.js
@@ -3,7 +3,7 @@ Blockly.Blocks['when_run'] = {
       this.appendDummyInput()
           .appendField("當運行時");
       this.setNextStatement(true, null);
-      this.setColour(120);
+      this.setColour('#51BE5B');
     }
 };
 Blockly.JavaScript['when_run'] = function(block) { return ""; };
@@ -14,7 +14,7 @@ Blockly.Blocks['step_forward'] = {
           .appendField("移動-向前");
       this.setPreviousStatement(true, null);
       this.setNextStatement(true, null);
-      this.setColour(230);
+      this.setColour('#5099FC');
     }
 };
 Blockly.JavaScript['step_forward'] = function(block) {
@@ -27,10 +27,9 @@ Blockly.Blocks['turn_x'] = {
           .appendField(new Blockly.FieldDropdown([["轉向-右方 ↻","right"], ["轉向-左方 ↺","left"]]), "value");
       this.setPreviousStatement(true, null);
       this.setNextStatement(true, null);
-      this.setColour(230);
+      this.setColour('#5099FC');
     }
 };
-
 Blockly.JavaScript['turn_x'] = function(block) {
     var dropdown_value = block.getFieldValue('value');
     return 'GAME.player.turn("' + dropdown_value + '")\n';
@@ -41,7 +40,7 @@ Blockly.Blocks['say_hi'] = {
       this.appendDummyInput()
           .appendField("打招呼");
       this.setPreviousStatement(true, null);
-      this.setColour(65);
+      this.setColour('#986BFC');
     }
 };
 Blockly.JavaScript['say_hi'] = function(block) {
